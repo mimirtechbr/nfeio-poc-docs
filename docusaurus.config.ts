db891dfed1064b0610b5b6857f5b312a9112c68d
Andre Kutianski <andre@eunarede.com>
2025-10-31 10:29:44 -0300

    chore: update production URL and base path for documentation site for testing purposes

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -22,10 +22,10 @@ const config: Config = {
     // Set the production url of your site here
     url: process.env.GH_PAGES_URL
         ? process.env.GH_PAGES_URL
-        : process.env.CI_PAGES_URL ?? "https://docs.nfe.io",
+        : process.env.CI_PAGES_URL ?? "https://nfe.io/docs",
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: '/',
+    baseUrl: '/docs/',
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
